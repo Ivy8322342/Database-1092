6e8cf3f3294b634ad081ac5afaf2c9a218597ca5
Ivy8322342 <a8322342tenacity@gmail.com>
2021-06-28 11:24:48 +0800

    想叫uber eat的時間，剛剛用了會員專區的back，陳沛竹正跟我開著賴通話

--- WindowFirst/src/demo_back.java
+++ WindowFirst/src/demo_back.java
@@ -5,6 +5,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 public class demo_back {
 	String Cid;
+	String passw;
+	String Name;
+	
+	ArrayList<String> CusInfo=new ArrayList<String>();
 	demo_back(String cid){
 		this.Cid=cid;
 	}
@@ -13,11 +17,13 @@ public class demo_back {
 		// TODO Auto-generated method stub
  demo_back demo=new demo_back("ivy20468");
  demo.CustomerInfo();
+ demo.edit("ivy204", "1123","vul","ÃÆ");
 	}
-	public ArrayList<String> CustomerInfo() throws SQLException{
-		ArrayList<String> CusInfo=new ArrayList<String>();
+	public String CustomerInfo() throws SQLException{
+		
 		Connection conn=null;
 		ResultSet result=null;
+		String info="";
 		
 		try {
 			conn=connection_sql.getConnection();
@@ -32,8 +38,11 @@ public class demo_back {
 			String passw=result.getString("Password");
 			String fN=result.getString("FirstName");
 			String LN=result.getString("LastName");
-			String info=id+","+passw+","+LN+fN;
-			CusInfo.add(info);
+			this.Cid=id;
+			this.Name=LN+fN;
+			this.passw=passw;
+		    info=id+","+passw+","+LN+","+fN;
+			
 			System.out.println(info);
 					
 			}
@@ -44,7 +53,83 @@ public class demo_back {
 		finally {
 			conn.close();
 		}
-		return CusInfo;
+		return info;
+	}
+	public void edit(String Newid,String NewPassw,String NewLn,String NewFn) throws SQLException {
+		String info=CustomerInfo();
+		System.out.println(info);
+		String[] s=info.split(",");
+		String oldcid=Cid;
+		String oldpass=passw;
+		String oldLn=s[2];
+		String oldFn=s[3];
+		System.out.println("oldid="+oldcid+"\noldpass="+oldpass+"oldName="+oldLn+oldFn+"/////////////");
+		Connection conn=null;
+		ResultSet result=null;
+		
+		
+		try {
+			conn=connection_sql.getConnection();
+			PreparedStatement stat1=conn.prepareStatement("UPDATE CUSTOMER "
+					+ "SET Customer_ID=? "
+					+ "WHERE Password=? "
+					+ "AND Customer_ID=?");
+			
+			PreparedStatement stat2=conn.prepareStatement("UPDATE CUSTOMER "
+					+ "SET Password=? "
+					+ "WHERE Password=?"
+					+ "AND Customer_ID=?");
+			
+			PreparedStatement stat3=conn.prepareStatement("UPDATE CUSTOMER "
+					+ "SET LastName=? "
+					+ "WHERE Password=? "
+					+ "AND Customer_ID=?");
+			PreparedStatement stat4=conn.prepareStatement("UPDATE CUSTOMER "
+					+ "SET FirstName=? "
+					+ "WHERE Password=? "
+					+ "AND Customer_ID=?");
+			stat1.setString(1, Newid);
+			stat1.setString(2, oldpass);
+			stat1.setString(3, oldcid);
+			
+			stat2.setString(1,NewPassw);
+			stat2.setString(2, oldpass);
+			stat2.setString(3, oldcid);
+			
+			stat3.setString(1, NewLn);
+			stat3.setString(2, oldpass);
+			stat3.setString(3, oldcid);
+			
+			stat4.setString(1, NewFn);
+			stat4.setString(2, oldpass);
+			stat4.setString(3, oldcid);
+			stat1.executeUpdate();
+			stat2.executeUpdate();
+			stat3.executeUpdate();
+			stat4.executeUpdate();
+			
+			
+			System.out.println("­×§ï¦¨¥\!!");
+			String newcid=Newid;
+			this.Cid=newcid;
+			String[] s1=CustomerInfo().split(",");
+			
+			String newpass=s1[1];
+			String newName=s1[2]+s1[3];
+			
+			this.passw=newpass;
+			this.Name=newName;
+			System.out.println("newid="+newcid+"\n newpass="+newpass+"\n newName="+newName+"/////////////");		
+			
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			conn.close();
+		}
+		
+		
 	}
 
 }
